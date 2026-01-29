e19284feb324fa927727a1ef116f697ea6e28640
sai nalage <saikrishnanalage@gmail.com>
2026-01-29 17:50:39 +0530

    3rd commit

--- first java project/src/App.java
+++ first java project/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("saikrishna");
+        System.out.println("new line added");
+
     }
 }
